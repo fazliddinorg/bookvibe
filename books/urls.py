5ff12654e09bc59feb223fd28279cfe4441116e1
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2025-06-29 16:48:30 +0900

    Fixed minor bugs and improved HTML

--- books/urls.py
+++ books/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path
-
-from books.views import BooksView, BookDetailView, AddReviewView, EditReviewView, ConfirmDeleteReviewView, DeleteReviewView
+from books.views import (
+    BooksView,
+    BookDetailView,
+    AddReviewView,
+    EditReviewView,
+    ConfirmDeleteReviewView,
+    DeleteReviewView,
+)
 
 app_name = "books"
+
 urlpatterns = [
-    path("", BooksView.as_view(), name="list"),
-    path("<int:id>/", BookDetailView.as_view(), name="detail"),
-    path("<int:id>/reviews/", AddReviewView.as_view(), name="reviews"),
-    path("<int:book_id>/reviews/<int:review_id>/edit/", EditReviewView.as_view(), name="edit-review"),
-    path(
-        "<int:book_id>/reviews/<int:review_id>/delete/confirm/",
-        ConfirmDeleteReviewView.as_view(),
-        name="confirm-delete-review"
-    ),
-    path(
-        "<int:book_id>/reviews/<int:review_id>/delete/",
-        DeleteReviewView.as_view(),
-        name="delete-review"
-    ),
+    path("", BooksView.as_view(), name="book-list"),
+    path("<int:id>/", BookDetailView.as_view(), name="book-detail"),
+    path("<int:id>/reviews/", AddReviewView.as_view(), name="review-add"),
+    path("<int:book_id>/reviews/<int:review_id>/edit/", EditReviewView.as_view(), name="review-edit"),
+    path("<int:book_id>/reviews/<int:review_id>/delete/confirm/", ConfirmDeleteReviewView.as_view(), name="review-delete-confirm"),
+    path("<int:book_id>/reviews/<int:review_id>/delete/", DeleteReviewView.as_view(), name="review-delete"),
 ]
