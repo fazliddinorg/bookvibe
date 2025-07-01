e57e8660d08566d7a094fa366dbf2264b51aa09a
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2025-07-01 17:23:42 +0900

    Fixed bugs

--- config/views.py
+++ config/views.py
@@ -1,19 +1,29 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 from books.models import BookReview
 
 
-def landing_page(request):
+def landing_page(request: HttpRequest) -> HttpResponse:
     return render(request, "landing.html")
 
 
-def home_page(request):
+def home_page(request: HttpRequest) -> HttpResponse:
     book_reviews = BookReview.objects.all().order_by('-created_at')
-    page_size = request.GET.get('page_size', 10)
+
+    try:
+        page_size = min(int(request.GET.get('page_size', 10)), 100)
+    except ValueError:
+        page_size = 10
+
     paginator = Paginator(book_reviews, page_size)
 
-    page_num = request.GET.get('page', 1)
-    page_object = paginator.get_page(page_num)
+    try:
+        page_num = int(request.GET.get('page', 1))
+    except ValueError:
+        page_num = 1
 
-    return render(request, "home.html", {"page_obj": page_object})
+    page_obj = paginator.get_page(page_num)
+
+    return render(request, "home.html", {"page_obj": page_obj})
